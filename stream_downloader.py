1e124cfae7b8ca165c1b400bd39d8493552d0a70
EvilRedHorse <39168275+EvilRedHorse@users.noreply.github.com>
2020-06-16 11:47:01 -0400

    Update stream_downloader.py

--- stream_downloader.py
+++ stream_downloader.py
@@ -17,9 +17,9 @@ def rmdir(dir):
 	if os.path.isdir(dir):
 		shutil.rmtree(dir)
 
-parser = argparse.ArgumentParser(description="Restream Youtube/Twitch live to SkyLive")
+parser = argparse.ArgumentParser(description="Restream Youtube/Twitch live to Public Portals")
 parser.add_argument('--url', help='Video url (for example https://www.youtube.com/watch?v=ASD123', required=True)
-# parser.add_argument('--token', help='SkyLive live stream token. You need to create a new stream on https://skylive.coolhd.hu', required=True)
+# parser.add_argument('--token', help='Public Portals live stream token. You need to create a new stream on https://scp.techandsupply.ca', required=True)
 args = parser.parse_args()
 
 # create temp folder
